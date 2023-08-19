a3df41fe8548b33aeac55a6765cd59af6666bf13
misskacie <121023187+misskacie@users.noreply.github.com>
2023-08-19 16:09:49 +1000

    create message page

--- src/routes/button.svelte
+++ src/routes/button.svelte
@@ -1,29 +1,55 @@
 <script>
-	
-	let buttonProps = {
-	class:[$$restProps.class]
-	}
+    export let buttonType = 'primary';
+    export let flat = false;
+    export let inverse = false;
+    export let disabled = false
+
 </script>
-	<button on:click
-			on:mouseover
-			on:mouseenter
-			on:mouseleave
-		{...buttonProps}>
-			<slot/>
-	</button>
+
+<button class={buttonType} class:flat={flat} class:inverse={inverse} class:disabled={disabled}>
+    <slot></slot>
+</button>
 
 <style>
-	.primary{
-		color:green;
-	}
-	.danger {
-		color:red;
-	}
-	.sm {
-		font-size:1em;
-		padding:0.1em;
-	}
-	.lg {
-		font-size:2em
-	}
+    button {
+        border: 0;
+        cursor: pointer;
+        border-radius: 6px;
+        padding: 8px 12px;
+        font-weight: bold;
+        box-shadow: 1px 2px 3px rgba(0,0,0,0.2);
+    }
+    .primary {
+		background: #45c496;
+        color: white
+        
+    }
+    .secondary {
+        background: #d91b42;
+        color: white
+    }
+    .flat {
+        box-shadow: none
+    }
+    .primary.inverse {
+        color: #d91b42;
+        background: white;
+        border: 2px solid #d91b42
+    }
+    .secondary.inverse {
+        color: #45c496;
+        background: white;
+        border: 2px solid #45c496
+    }
+    .primary.disabled {
+        pointer-events: none;
+        background-color: whitesmoke;
+        color: lightgrey
+    }
+    .secondary.disabled {
+        pointer-events: none;
+        background-color: whitesmoke;
+        color: lightgrey
+    }
+
 </style>
--- src/routes/messages/+page.svelte
+++ src/routes/messages/+page.svelte
@@ -1,2 +1,93 @@
-<h1>Messages page</h1>
-<p> </p>
+
+<script>
+	import Button from "../button.svelte"
+   
+</script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<body>
+    
+    <nav class="nav">
+        
+    <button class="btn"> <a href="/"><i class="fa fa-home"></i></a></button>
+    <button class="btn"><a href="/notifications"><i class="fa fa-bell-o"></i></a></button>
+    <button class="btn"><a href="/profile"><i class="fa fa-user-circle-o"></i></a></button>
+    <button class="btn"><a href="/settings"><i class="fa fa-cog"></i></a></button>
+    <button class="btn"><i class="fa fa-search"></i></button>
+     
+        
+    </nav>
+
+    <section class="home-page">
+        <h1>FEED</h1>
+    </section>
+
+    <section class="search">
+        <h1>SEARCH</h1>
+    </section>
+</body>
+
+<style>
+    a {
+        text-decoration: none;
+
+    }
+    
+    .nav {
+        position: fixed;
+        top: 0;
+        left: 0;
+        padding-left: 0px;
+        width: 50px;
+        height: 100%;
+        background-color: #D7BBF5;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .nav-link {
+        position: relative;
+    }
+
+    .nav-hover {
+        top: 0;
+        left: 0;
+        width: 100px;
+        height: 100%;
+        background-color: #D7BBF5;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        color: white;
+        padding-top: 30px;
+        padding-bottom: 30px;
+    }
+
+    .navlink:hover {
+        background-color: light-grey;
+    }
+
+    .home-page {
+        position: absolute;
+        top: 0;
+        left: 50px;
+        margin-left: 50px;
+        width: 900px;
+        height: 100%;
+        background-color: #D4E2D4;
+    }
+
+    .search {
+        position: absolute;
+        top: 0;
+        left: 50px;
+        margin-left: 1000px;
+        width: 200px;
+        height: 100%;
+        background-color: #FFCEFE;
+    }
+
+</style>
