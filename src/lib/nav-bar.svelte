aa54deeaaf28a0595ddea3af5d0b8d7606d6d1b4
alie-nel <natalienguyen33@gmail.com>
2023-08-19 14:17:19 +1000

    update home page

---
+++ src/lib/nav-bar.svelte
@@ -0,0 +1,59 @@
+<style>
+    body {
+        background-color: #D7BBF5;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .nav-link {
+        position: relative;
+        background-color: pink;
+        margin: 15px 0;
+        padding: 20px 10px;
+    }
+
+    .nav-hover {
+        top: 0;
+        left: 0;
+        right: 0;
+        width: 100px;
+        height: 100%;
+        background-color: #D7BBF5;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        color: white;
+        padding-top: 30px;
+        padding-bottom: 30px;
+    }
+
+    .navlink:hover {
+        background-color: light-grey;
+    }
+
+    .home-page {
+        position: absolute;
+        top: 0;
+        left: 50px;
+        margin-left: 150px;
+        width: 600px;
+        height: 100%;
+        background-color: blue;
+        border-radius: 15px;
+    }
+
+    .search {
+        position: absolute;
+        top: 0;
+        left: 50px;
+        margin-left: 800px;
+        width: 400px;
+        height: 100%;
+        background-color: pink;
+        border-radius: 15px;
+    }
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,82 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import NavBar from "../lib/nav-bar.svelte"
+</script>
 
-<body>
-    <nav class="nav">
-        <div class="nav-link">home</div>
-        <div class="nav-link">search</div>
-        <div class="nav-link">notifications</div>
-        <div class="nav-link">messages</div>
-        <div class="nav-link">profile</div>
-        <div class="nav-link">house</div>
-    </nav>
-
-    <section class="home-page">
-        <h1>FEED</h1>
-    </section>
-
-    <section class="search">
-        <h1>SEARCH</h1>
-    </section>
-</body>
-
-<style>
-    
-    .nav {
-        position: fixed;
-        top: 0;
-        left: 0;
-        padding-left: 50px;
-        width: 100px;
-        height: 100%;
-        background-color: #D7BBF5;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-    }
-
-    .nav-link {
-        position: relative;
-    }
-
-    .nav-hover {
-        top: 0;
-        left: 0;
-        width: 100px;
-        height: 100%;
-        background-color: #D7BBF5;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        color: white;
-        padding-top: 30px;
-        padding-bottom: 30px;
-    }
-
-    .navlink:hover {
-        background-color: light-grey;
-    }
-
-    .home-page {
-        position: absolute;
-        top: 0;
-        left: 50px;
-        margin-left: 150px;
-        width: 600px;
-        height: 100%;
-        background-color: blue;
-    }
-
-    .search {
-        position: absolute;
-        top: 0;
-        left: 50px;
-        margin-left: 750px;
-        width: 300px;
-        height: 100%;
-        background-color: pink;
-    }
-
-</style>
+<main>
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</main>
+<NavBar/>
